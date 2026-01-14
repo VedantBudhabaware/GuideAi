421747902e4581d0452a115b4673ace2f50e3f90
Vedant <vedantbudhabaware@gmail.com>
2026-01-14 19:11:39 +0530

    10: Responsive dialog

---
+++ src/components/responsive-dialog.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle } from "@/components/ui/drawer";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { ReactNode, useId } from "react";
+
+interface ResponsiveDialogProps {
+    title: string;
+    description: string;
+    children: React.ReactNode;
+    open: boolean;
+    onOpenChange: (open: boolean) => void;
+}
+
+export const ResponsiveDialog = ({ title, description, children, open, onOpenChange }: ResponsiveDialogProps) => {
+    const isMobile = useIsMobile();
+    const titleId = useId();
+
+    if (isMobile) {
+        return (
+            <Drawer open={open} onOpenChange={onOpenChange}>
+                <DrawerContent>
+                    <DrawerHeader>
+                        <DrawerTitle>{title}</DrawerTitle>
+                        <DrawerDescription>{description}</DrawerDescription>
+                    </DrawerHeader>
+                    <section className="p-4">{children}</section>
+                </DrawerContent>
+            </Drawer>
+        );
+    }
+
+    return (
+        <Dialog open={open} onOpenChange={onOpenChange}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>{title}</DialogTitle>
+                    <DialogDescription>{description}</DialogDescription>
+                </DialogHeader>
+                <section>{children}</section>
+            </DialogContent>
+        </Dialog>
+    );
+};
